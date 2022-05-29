fd19b796e8d5a94867315965cef545aa8fce2ad6
irfansyah06 <maulana.bintang.irfansyah@gmail.com>
2022-05-29 13:32:18 +0700

    Menambahkan Halaman Login

--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,6 @@ Route::get('/', function () {
 });
 
 Auth::routes();
+Route::get('/', [HomeController::class,'index'])->name('home');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
