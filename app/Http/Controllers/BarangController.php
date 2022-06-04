fb81ae6fe4b9d39accd6e725583accc59a52aba3
gustaaa <gustayo24@gmail.com>
2022-06-04 20:08:10 +0700

    Menambahkan function store, show, edit, update, destroy, dan laporan pada BarangController

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
 use RealRashid\SweetAlert\Facades\Alert;
 use PDF;
+use Illuminate\Support\Facades\Storage;
 
 class BarangController extends Controller
 {
@@ -67,7 +68,44 @@ class BarangController extends Controller
      */
     public function store(Request $request)
     {
-        
+        if(Auth::user()->role == 'Operator') {
+            Alert::info('Oopss..', 'Anda dilarang masuk ke area ini.');
+            return redirect()->to('/barang');
+        }
+
+        //melakukan validasi data
+        $request->validate([
+            'kode_barang' => 'required',
+            'nama_barang' => 'required',
+            'gambar' => 'required',
+            'jumlah_barang' => 'required',
+            'id_kategori' => 'required',
+            'id_supplier' => 'required',
+            ]);
+
+            if ($request->file('gambar')) {
+                $image_name = $request->file('gambar')->store('images', 'public');
+            }
+
+            $kategori = Kategori::find($request->get('id_kategori'));
+            $supplier = Supplier::find($request->get('id_supplier'));
+
+            $barang = new Barang;
+            $barang->kode_barang = $request->get('kode_barang');
+            $barang->nama_barang = $request->get('nama_barang');
+            $barang->gambar = $image_name;
+            $barang->jumlah_barang = $request->get('jumlah_barang');
+
+            //fungsi eloquent untuk menambah data dengan relasi belongsTo
+            $barang->kategori()->associate($kategori);
+            $barang->supplier()->associate($supplier);
+            $barang->save();
+
+            //jika data berhasil ditambahkan, akan kembali ke halaman utama
+            Alert::success('Success', 'Data Barang Berhasil Ditambahkan');
+            return redirect()->route('barang.index');
+
+
     }
 
     /**
@@ -78,7 +116,13 @@ class BarangController extends Controller
      */
     public function show($id)
     {
-        
+        if(Auth::user()->role == 'Operator') {
+            Alert::info('Oopss..', 'Anda dilarang masuk ke area ini.');
+            return redirect()->to('/barang');
+        }
+
+        $barang = Barang::with('kategori','supplier')->find($id);
+        return view('Barang.show', compact('barang'));
     }
 
     /**
@@ -89,7 +133,15 @@ class BarangController extends Controller
      */
     public function edit($id)
     {
-        
+        if(Auth::user()->role == 'Operator') {
+            Alert::info('Oopss..', 'Anda dilarang masuk ke area ini.');
+            return redirect()->to('/barang');
+        }
+
+        $barang = Barang::with('kategori','supplier')->find($id);
+        $kategori = Kategori::all();
+        $supplier = Supplier::all();
+        return view('Barang.edit', compact('barang', 'kategori', 'supplier'));
     }
 
     /**
@@ -101,7 +153,55 @@ class BarangController extends Controller
      */
     public function update(Request $request, $id)
     {
-        
+        if(Auth::user()->role == 'Operator') {
+            Alert::info('Oopss..', 'Anda dilarang masuk ke area ini.');
+            return redirect()->to('/barang');
+        }
+
+        $request->validate([
+            'kode_barang' => 'required',
+            'nama_barang' => 'required',
+            'jumlah_barang' => 'required',
+            'id_kategori' => 'required',
+            'id_supplier' => 'required',
+        ]);
+
+        $barang = Barang::with('kategori', 'supplier')->where('id', $id)->first();
+
+        if ($request->file('gambar') == ''){
+            $barang->kode_barang = $request->get('kode_barang');
+            $barang->nama_barang = $request->get('nama_barang');
+            $barang->jumlah_barang = $request->get('jumlah_barang');
+
+            $kategori = Kategori::find($request->get('id_kategori'));
+            $supplier = Supplier::find($request->get('id_supplier'));
+            //fungsi eloquent untuk menambah data dengan relasi belongsTo
+            $barang->kategori()->associate($kategori);
+            $barang->supplier()->associate($supplier);
+            $barang->save();
+        } else{
+            if ($barang->gambar && file_exists(storage_path('app/public/' .$barang->gambar)))
+            {
+                Storage::delete(['public/' . $barang->gambar]);
+            }
+            $image_name = $request->file('gambar')->store('images', 'public');
+            $barang->gambar = $image_name;
+
+            $barang->kode_barang = $request->get('kode_barang');
+            $barang->nama_barang = $request->get('nama_barang');
+            $barang->jumlah_barang = $request->get('jumlah_barang');
+
+            $kategori = Kategori::find($request->get('id_kategori'));
+            $supplier = Supplier::find($request->get('id_supplier'));
+            //fungsi eloquent untuk menambah data dengan relasi belongsTo
+            $barang->kategori()->associate($kategori);
+            $barang->supplier()->associate($supplier);
+            $barang->save();
+        }
+
+        //jika data berhasil ditambahkan, akan kembali ke halaman utama
+        Alert::success('Success', 'Data Barang Berhasil Diedit');
+        return redirect()->route('barang.index');
     }
 
     /**
@@ -112,7 +212,22 @@ class BarangController extends Controller
      */
     public function destroy($id)
     {
-        
+        if(Auth::user()->role == 'Operator') {
+            Alert::info('Oopss..', 'Anda dilarang masuk ke area ini.');
+            return redirect()->to('/barang');
+        }
+
+        Barang::find($id)->delete();
+        Alert::success('Success', 'Data Barang berhasil dihapus');
+        return redirect()->route('barang.index');
     }
 
+    public function laporan()
+    {
+        $barang = Barang::all();
+        $kategori = Kategori::all();
+        $supplier = Supplier::all();
+        $pdf = PDF::loadview('Barang.laporan', compact('barang', 'kategori', 'supplier'));
+        return $pdf->stream();
+    }
 }
