9b9a2a9ccddda1f6a8a51ad77d5df5235b79ada2
irfansyah06 <maulana.bintang.irfansyah@gmail.com>
2022-06-05 10:07:07 +0700

    Menambahkan function construct, index, dan create pada UserController

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Hash;
+use RealRashid\SweetAlert\Facades\Alert;
+use PDF;
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware(function($request, $next){
+        if(Gate::allows('manage-MasterData')) return $next($request);
+        abort(403, 'Anda tidak memiliki cukup hak akses');
+        });
+    }
+
+    public function index(Request $request)
+    {
+        if($request->has('search')){ // Pemilihan jika ingin melakukan pencarian
+            $user = User::where('name', 'like', "%" . $request->search . "%")
+            ->orwhere('email', 'like', "%" . $request->search . "%")
+            ->orwhere('role', 'like', "%" . $request->search . "%")
+            ->paginate();
+            return view('User.index', compact('user'))->with('i', (request()->input('page', 1) - 1) * 5);
+        } else { // Pemilihan jika tidak melakukan pencarian
+            //fungsi eloquent menampilkan data menggunakan pagination
+            $user = User::paginate(10); // MenPagination menampilkan 5 data
+            return view('User.index', compact('user'));
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('User.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+
+    }
+}
